95660bef2e030a5bbe40c8041839a951bf70d358
Muzaffar-Xadjayev <muzaffar010125@gmail.com>
2023-12-18 23:15:59 +0500

    create category model

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    """ Category """
+    name = models.CharField(max_length=255, primary_key=True)
+    description = models.TextField()
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
