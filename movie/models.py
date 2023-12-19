9d6b1c7d64bef5800f5b46f4bd8456d5a487115c
Muzaffar-Xadjayev <muzaffar010125@gmail.com>
2023-12-19 23:29:17 +0500

    add actor and genre movie

--- movie/models.py
+++ movie/models.py
@@ -12,3 +12,24 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+
+class Actor(models.Model):
+    """ Actor """
+    name = models.CharField(max_length=255)
+    birt_date = models.DateField()
+    description = models.TextField()
+    image = models.ImageField(upload_to="actors/")
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    """ Genres """
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    url = models.SlugField(max_length=155, unique=True)
+
+    def __str__(self):
+        return self.name
+
