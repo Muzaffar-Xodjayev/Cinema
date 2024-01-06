d6aa97b5819d9e53abc7cab390936d60c7a61e87
Muzaffar-Xadjayev <muzaffar010125@gmail.com>
2024-01-06 23:07:42 +0500

    add models in admin panel

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Movie)
+admin.site.register(MovieShot)
+admin.site.register(Actor)
+admin.site.register(Rating)
+admin.site.register(RatingStar)
+admin.site.register(Review)
